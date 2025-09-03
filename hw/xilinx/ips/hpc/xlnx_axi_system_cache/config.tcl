a8eb83d8162e3292e8b29823e0de3a9cf2e73b58
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2025-09-03 20:29:11 +0200

    First configuration

---
+++ hw/xilinx/ips/hpc/xlnx_axi_system_cache/config.tcl
@@ -0,0 +1,13 @@
+create_ip -name system_cache -vendor xilinx.com -library ip -version 5.0 -module_name $::env(IP_NAME)
+
+set_property -dict [list \
+  CONFIG.C_FREQ {300.0} \
+  CONFIG.C_NUM_OPTIMIZED_PORTS {0} \
+  CONFIG.C_NUM_GENERIC_PORTS {1} \
+  CONFIG.C_S0_AXI_GEN_DATA_WIDTH {512} \
+] [get_ips $::env(IP_NAME)]
+
+set_property CONFIG.C_S0_AXI_GEN_ADDR_WIDTH $::env(MBUS_ADDR_WIDTH)  [get_ips $::env(IP_NAME)]
+set_property CONFIG.C_S0_AXI_GEN_ID_WIDTH $::env(MBUS_ID_WIDTH)  [get_ips $::env(IP_NAME)]
+
+
