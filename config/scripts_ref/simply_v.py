b4637d5363b4d469e714335a7c4654f4a86ed6a6
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2025-09-18 23:23:46 +0200

    General recursive structure working with MBus, PBus and Peripherals (ADDR_RANGES managment need to be fixed + some parameters checks need to be added)

--- config/scripts_ref/simply_v.py
+++ config/scripts_ref/simply_v.py
@@ -1,11 +1,13 @@
 import mbus
-import utils
-import logging
+from pprint import pprint
+from utils import *
+from logger import Logger
 
 class SimplyV:
 	def __init__(self, sys_config_file_name: str, mbus_file_name: str, soc_profile: str):
 		# defaults
-		self.SUPPORTED_CORES : list = ["CORE_PICORV32", "CORE_CV32E40P", "CORE_IBEX", "CORE_MICROBLAZEV", "CORE_CV64A6"]
+		self.SUPPORTED_CORES : list = ["CORE_PICORV32", "CORE_CV32E40P", "CORE_IBEX", "CORE_MICROBLAZEV_RV32", \
+										"CORE_MICROBLAZEV_RV64", "CORE_CV64A6"]
 		self.EMBEDDED_SUPPORTED_CLOCKS : list = [10, 20, 50, 100]
 		self.HPC_SUPPORTED_CLOCKS : list = [10, 20, 50, 100, 250]
 		self.CORE_SELECTOR : str
@@ -13,80 +15,90 @@ class SimplyV:
 		self.XLEN : int = 32
 		self.PHYSICAL_ADDR_WIDTH : int = 32
 		self.MAIN_CLOCK_DOMAIN : int
+		self.logger = Logger(sys_config_file_name)
 		self.soc_profile : str = soc_profile
-		self.mbus : Bus
-
+		self.mbus : MBus
+		
 		# read config file
-		data_dict = parse_csv(sys_config_file_name)
+		system_data_dict = parse_csv(sys_config_file_name)
 
 		# check and assign parameters
-		self.check_assign_params(data_dict)
+		self.check_assign_params(system_data_dict)
+
+		# check params interactions
+		self.check_intra()
 
 		# Create root node (MBUS)
-		range_addr_width = self.PHYSICAL_ADDR_WIDTH
-		data_width = self.XLEN
 		addr_width = self.PHYSICAL_ADDR_WIDTH
+		data_width = self.XLEN
+		# this is a list of len ADDR_RANGES
+		range_addr_width = [self.PHYSICAL_ADDR_WIDTH]
 		clock = self.MAIN_CLOCK_DOMAIN
 
-		self.mbus = mbus.MBus(mbus_file_name, range_addr_width, data_width, addr_width, clock)
+		mbus_data_dict = parse_csv(mbus_file_name)
+
+		self.mbus = mbus.MBus(mbus_data_dict, mbus_file_name, addr_width, data_width, range_addr_width, clock)
+
+		pprint(vars(self.mbus))
+
 
 	def check_assign_params(self, data_dict: dict):
+		simply_v_crash = self.logger.simply_v_crash
+
 		if ("CORE_SELECTOR" not in data_dict):
-			logging.error("CORE_SELECTOR is mandatory")
-			exit(1)
+			simply_v_crash("CORE_SELECTOR is mandatory")
 
 		self.CORE_SELECTOR = data_dict["CORE_SELECTOR"]
 		
 		if (self.CORE_SELECTOR not in self.SUPPORTED_CORES):
-			logging.error("CORE_SELECTOR unsupported")
-			exit(1)
+			simply_v_crash("CORE_SELECTOR unsupported")
 
 		if ("VIO_RESETN_DEFAULT" in data_dict):
 			self.VIO_RESETN_DEFAULT = int(data_dict["VIO_RESETN_DEFAULT"])
 			if ((self.VIO_RESETN_DEFAULT != 0) and (self.VIO_RESETN_DEFAULT != 1)):
-				logging.error("VIO_RESETN_DEFAULT unexpected value")
-				exit(1)
-
+				simply_v_crash("VIO_RESETN_DEFAULT unexpected value")
+	
 		if ("XLEN" in data_dict):
 			self.XLEN = int(data_dict["XLEN"])
 			if ((self.XLEN != 32) and (self.XLEN != 64)):
-				logging.error("XLEN unexpected value")
-				exit(1)
+				simply_v_crash("XLEN unexpected value")
 
 		if ("PHYSICAL_ADDR_WIDTH" in data_dict):
 			self.PHYSICAL_ADDR_WIDTH = int(data_dict["PHYSICAL_ADDR_WIDTH"])
-			if(self.PHYSICAL_ADDR_WIDTH < 32):
-				logging.error("PHYSICAL_ADDR_WIDTH can't be less then 32")
-				exit(1)
-
-			if((self.XLEN == 32) and (self.PHYSICAL_ADDR_WIDTH != 32))
-				logging.error("PHYSICAL_ADDR_WIDTH doesn't match when XLEN = 32")
-				exit(1)
-
-			if((self.XLEN == 64) and ((self.PHYSICAL_ADDR_WIDTH == 32) or (self.PHYSICAL_ADDR_WIDTH > 64)))
-				logging.error("PHYSICAL_ADDR_WIDTH should be in range (32,64] when XLEN = 64")
-				exit(1)
+			if(self.PHYSICAL_ADDR_WIDTH not in range(32,65)):
+				simply_v_crash("PHYSICAL_ADDR_WIDTH can't be outside [32,64]")
 
 		if ("MAIN_CLOCK_DOMAIN" not in data_dict):
-			logging.error("MAIN_CLOCK_DOMAIN is mandatory")
-			exit(1)
+			simply_v_crash("MAIN_CLOCK_DOMAIN is mandatory")
 
-		self.MAIN_CLOCK_DOMAIN = data_dict["MAIN_CLOCK_DOMAIN"]
+		self.MAIN_CLOCK_DOMAIN = int(data_dict["MAIN_CLOCK_DOMAIN"])
 
+
+	def check_intra(self):
+		simply_v_crash = self.logger.simply_v_crash
+
+		# check XLEN and PHYSICAL_ADDR_WIDTH interaction
+		if((self.XLEN == 32) and (self.PHYSICAL_ADDR_WIDTH != 32)):
+			simply_v_crash("PHYSICAL_ADDR_WIDTH doesn't match when XLEN = 32")
+
+		# check XLEN with MicroblazeV type interaction
+		if ((self.CORE_SELECTOR == "CORE_MICROBLAZEV_RV64" and self.XLEN == 32) or \
+			(self.CORE_SELECTOR == "CORE_MICROBLAZEV_RV32" and self.XLEN == 64)):
+			simply_v_crash(f"XLEN={self.XLEN} doesn't match {self.CORE_SELECTOR} data width.")
+
+		if((self.XLEN == 64) and ((self.PHYSICAL_ADDR_WIDTH == 32) or (self.PHYSICAL_ADDR_WIDTH > 64))):
+			simply_v_crash("PHYSICAL_ADDR_WIDTH should be in range (32,64] when XLEN = 64")
+
+		# check profile and MAIN_CLOCK_DOMAIN interaction
 		if (self.soc_profile == "embedded"):
 			if (self.MAIN_CLOCK_DOMAIN not in self.EMBEDDED_SUPPORTED_CLOCKS):
-				logging.error(f"Unsupported MAIN_CLOCK_DOMAIN value (supported values\
+				simply_v_crash(f"Unsupported MAIN_CLOCK_DOMAIN value (supported values\
 						for {self.soc_profile} profile: {self.EMBEDDED_SUPPORTED_CLOCKS})")
-				exit(1)
 		else: #hpc
 			if (self.MAIN_CLOCK_DOMAIN not in self.HPC_SUPPORTED_CLOCKS):
-				logging.error(f"Unsupported MAIN_CLOCK_DOMAIN value (supported values\
+				simply_v_crash(f"Unsupported MAIN_CLOCK_DOMAIN value (supported values\
 						for {self.soc_profile} profile: {self.HPC_SUPPORTED_CLOCKS})")
-				exit(1)
 
-
-
-		
-		
-
-		
+		## check CORE_SELECTOR and VIO_RESETN_DEFAULT interaction
+		if self.CORE_SELECTOR == "CORE_PICORV32" and self.VIO_RESETN_DEFAULT != 0:
+			simply_v_crash(f"CORE_PICORV32 only supports VIO_RESETN_DEFAULT = 0! {self.VIO_RESETN_DEFAULT}")
--- config/scripts_ref/utils.py
+++ config/scripts_ref/utils.py
@@ -1,11 +1,14 @@
+import pandas as pd
+
 def parse_csv(file_name: str) -> dict:
 	try:
 		data_set = pd.read_csv(file_name, sep=",")
-		return dict(zip(data_set["Property"], data_set["Value"]))
+		data_dict = dict(zip(data_set["Property"], data_set["Value"]))
+		return data_dict
 	except (FileNotFoundError, ParserError, Exception) as e:
-        messages = {
-            FileNotFoundError: f"File {file_name} not found",
-            ParserError: f"Malformed format in {file_name}"
-        }
-        logging.error(messages.get(type(e), f"Unexpected error with {file_name}"))
-        exit(1)
+		messages = {
+			FileNotFoundError: f"File {file_name} not found",
+			ParserError: f"Malformed format in {file_name}"
+		}
+		logging.error(messages.get(type(e), f"Unexpected error with {file_name}"))
+		exit(1)
