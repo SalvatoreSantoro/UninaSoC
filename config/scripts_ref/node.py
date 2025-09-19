eba42e8896a39c9c4ad7b13f018e7cb4889a8aa3
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2025-09-19 20:42:03 +0200

    Fixed ADDR_RANGES + implemented check_peripherals

--- config/scripts_ref/node.py
+++ config/scripts_ref/node.py
@@ -1,10 +1,10 @@
 from abc import ABC
 
 class Node(ABC):
-	def __init__(self, assigned_addr_ranges: int, base_addr: list, range_addr_width: list, clock: int):
-		self.ASSIGNED_ADDR_RANGES : int =assigned_addr_ranges 
-		self.RANGE_BASE_ADDR : list = base_addr
-		self.RANGE_ADDR_WIDTH : list = range_addr_width 
+	def __init__(self, asgn_addr_ranges: int, asgn_range_base_addr: list, asgn_range_addr_width: list, clock: int):
+		self.ASGN_ADDR_RANGES : int = asgn_addr_ranges 
+		self.ASGN_RANGE_BASE_ADDR : list = asgn_range_base_addr 
+		self.ASGN_RANGE_ADDR_WIDTH : list = asgn_range_addr_width 
 		self.CLOCK: int = clock
 		
 
--- config/scripts_ref/pbus.py
+++ config/scripts_ref/pbus.py
@@ -1,22 +1,36 @@
 from bus import Bus
 import peripheral
+from pprint import pprint
 
 class PBus(Bus):
-	def __init__(self, data_dict: dict, pbus_file_name: str, range_addr_width: list, base_addr: list, \
-			assigned_addr_ranges: int, clock: int):
+	def __init__(self, pbus_data_dict: dict, pbus_file_name: str, asgn_addr_ranges: int, asgn_range_base_addr: list, \
+			asgn_range_addr_width: list, clock: int):
 
-		self.LEGAL_PERIPHERALS: list = []
+		self.LEGAL_PERIPHERALS: list = ["UART", "GPIO_out", "GPIO_in", "TIM"]
 		self.PROTOCOL: str = "AXI4LITE"
 		self.ADDR_WIDTH: int = 32
 		self.DATA_WIDTH: int = 32
 
-		super().__init__(data_dict, pbus_file_name, base_addr, assigned_addr_ranges, range_addr_width, clock)
+
+		super().__init__(pbus_data_dict, pbus_file_name, asgn_addr_ranges, asgn_range_base_addr, \
+				   asgn_range_addr_width, clock)
+
+		self.check_intra()
+
+		self.check_peripherals(self.LEGAL_PERIPHERALS)
 
 		self.generate_children()
 
+	def check_intra(self):
+		#check params interactions of BUS object
+		super().check_intra()
+		
+
 	def generate_children(self):
 		for i, node_name in enumerate(self.RANGE_NAMES):
-			node = peripheral.Peripheral(self.RANGE_NAMES[i], self.RANGE_ADDR_WIDTH_LIST[i], \
-						self.RANGE_BASE_ADDR_LIST[i], self.ADDR_RANGES, self.CLOCK)
+			node = peripheral.Peripheral(self.RANGE_NAMES[i], self.ADDR_RANGES, \
+						self.RANGE_BASE_ADDR[i:(i+self.ADDR_RANGES)], \
+						self.RANGE_ADDR_WIDTH[i:(i+self.ADDR_RANGES)], \
+						self.CLOCK)
+			pprint(vars(node))
 			self.children.append(node)
-
