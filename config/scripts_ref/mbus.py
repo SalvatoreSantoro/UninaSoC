5cbc4280df0f06bc733ce20b781b4a1a09344c3d
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2025-09-19 20:42:03 +0200

    Fixed ADDR_RANGES + implemented check_peripherals

--- config/scripts_ref/mbus.py
+++ config/scripts_ref/mbus.py
@@ -7,32 +7,39 @@ import peripheral
 import re
 
 class MBus(Bus):
-	def __init__(self, data_dict: dict, mbus_file_name: str, addr_width: int, data_width: int, range_addr_width: list, clock: int):
-		self.LEGAL_PERIPHERALS: list = []
-		self.PROTOCOL: str = "AXI4"
-		self.ADDR_WIDTH: int = addr_width
-		self.DATA_WIDTH: int = data_width
-		self.RANGE_CLOCK_DOMAINS: list = []
-		self.mbus_file_name = mbus_file_name
 
-		# MBUS has a base address of 0 as a convention
-		base_addr = [0]
-		# MBUS has an addr ranges of 1 as a convention
-		assigned_addr_ranges = 1
+	def __init__(self, mbus_data_dict: dict, mbus_file_name: str, axi_addr_width: int, axi_data_width: int, \
+			asgn_addr_ranges: int, asgn_range_base_addr: list, asgn_range_addr_width: list, clock: int):
+
+		self.LEGAL_PERIPHERALS: list = ["BRAM", "DM_mem", "PBUS", "PLIC"]
+		self.PROTOCOL: str = "AXI4"
+		self.ADDR_WIDTH: int = axi_addr_width
+		self.DATA_WIDTH: int = axi_data_width
+		self.RANGE_CLOCK_DOMAINS: list = []
+
 		
 		# init Bus object
-		super().__init__(data_dict, mbus_file_name, base_addr, assigned_addr_ranges, range_addr_width, clock)
+		super().__init__(mbus_data_dict, mbus_file_name, asgn_addr_ranges, asgn_range_base_addr, \
+				   asgn_range_addr_width, clock)
 
-		self.RANGE_CLOCK_DOMAINS = [int(x) for x in data_dict["RANGE_CLOCK_DOMAINS"].split(" ")]
+		self.RANGE_CLOCK_DOMAINS = [int(x) for x in mbus_data_dict["RANGE_CLOCK_DOMAINS"].split(" ")]
+
+		self.check_intra()
+
+		self.check_peripherals(self.LEGAL_PERIPHERALS)
 
 		#generate children nodes
 		self.generate_children()
 
+	def check_intra(self):
+		#check params interactions of BUS object
+		super().check_intra()
 
 	def generate_children(self):
-		pbus_pattern = r"PBUS_\d+"   # \d+ matches one or more digits
-		hbus_pattern = r"HBUS_\d+"   # \d+ matches one or more digits
-		root_dir = os.path.join("/", *self.mbus_file_name.split("/")[:-1])
+		pbus_pattern = r"PBUS(?:_\d+)?"
+		hbus_pattern = r"HBUS(?:_\d+)?"
+
+		root_dir = os.path.join("/", *self.file_name.split("/")[:-1])
 		
 		for i, node_name in enumerate(self.RANGE_NAMES):
 			match = re.search(pbus_pattern, node_name)
@@ -40,8 +47,11 @@ class MBus(Bus):
 				pbus_file_name = os.path.join(root_dir, "config_" + match.group().lower() + ".csv")
 				pbus_data_dict = parse_csv(pbus_file_name)
 
-				node = pbus.PBus(pbus_data_dict, pbus_file_name, [self.RANGE_ADDR_WIDTH_LIST[i]], \
-						[self.RANGE_BASE_ADDR_LIST[i]], self.ADDR_RANGES, self.RANGE_CLOCK_DOMAINS[i])
+				node = pbus.PBus(pbus_data_dict, pbus_file_name, self.ADDR_RANGES, \
+						self.RANGE_BASE_ADDR[i:(i+self.ADDR_RANGES)], \
+						self.RANGE_ADDR_WIDTH[i:(i+self.ADDR_RANGES)], \
+						self.RANGE_CLOCK_DOMAINS[i])
+
 				pprint(vars(node))
 				self.children.append(node)
 				continue
@@ -56,8 +66,10 @@ class MBus(Bus):
 				self.children.append(node)
 				continue
 
-			node = peripheral.Peripheral(self.RANGE_NAMES[i], self.ADDR_RANGES, [self.RANGE_BASE_ADDR_LIST[i]],\
-					[self.RANGE_ADDR_WIDTH_LIST[i]], self.RANGE_CLOCK_DOMAINS[i])
+			node = peripheral.Peripheral(self.RANGE_NAMES[i], self.ADDR_RANGES, \
+						self.RANGE_BASE_ADDR[i:(i+self.ADDR_RANGES)], \
+						self.RANGE_ADDR_WIDTH[i:(i+self.ADDR_RANGES)], \
+						self.RANGE_CLOCK_DOMAINS[i])
 
 			pprint(vars(node))
 			self.children.append(node)
