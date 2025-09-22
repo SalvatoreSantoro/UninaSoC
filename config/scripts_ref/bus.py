a80e7bfd27194753e80e9af04fcaaa33a2c9cfa5
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2025-09-22 23:37:34 +0200

    Implemented checks on protocol

--- config/scripts_ref/bus.py
+++ config/scripts_ref/bus.py
@@ -1,37 +1,45 @@
 from node import Node
-from collections import defaultdict
-import re
 from logger import Logger
 from utils import *
+
+from pprint import pprint
+
+from collections import defaultdict
+import re
 from abc import abstractmethod, ABC
 
 
 class Bus(Node, ABC):
-	def __init__(self, data_dict: dict, file_name: str, asgn_addr_ranges: int, \
-			asgn_range_base_addr: list, asgn_range_addr_width: list, clock: int):
+	def __init__(self, data_dict: dict, file_name: str, axi_addr_width: int, axi_data_width: int, \
+			  asgn_addr_ranges: int, asgn_range_base_addr: list, asgn_range_addr_width: list, clock: int):
 
 		self.ID_WIDTH			 : int = 4		# ID Data Width for MI and SI (a subset of it is used by the Interfaces Thread IDs)
-		self.NUM_MI				 : int			# Master Interface (MI) Number
-		self.NUM_SI				 : int	 		# Slave Interface (SI) Number
-		self.MASTER_NAMES        : list		    # List of names of masters connected to the bus
-		self.RANGE_NAMES         : list		    # List of names of slaves connected to the bus
-		self.file_name = file_name
-		self.logger = Logger(file_name)		# Utils object used for logging
-		self.children : list = []
+		self.NUM_MI				 : int = 0			# Master Interface (MI) Number
+		self.NUM_SI				 : int = 0	 		# Slave Interface (SI) Number
+		self.MASTER_NAMES        : list[str]		    # List of names of masters connected to the bus
+		self.RANGE_NAMES         : list[str]		    # List of names of slaves connected to the bus
+		self.PROTOCOL: str
+		self.ADDR_WIDTH: int = axi_addr_width
+		self.DATA_WIDTH: int = axi_data_width
+		self.file_name: str = file_name
+		self.logger: Logger = Logger(file_name)		# Utils object used for logging
+		self.children : list[Node] = []
 
 		self.ADDR_RANGES : int = 1 
-		self.RANGE_BASE_ADDR : list
-		self.RANGE_ADDR_WIDTH : list
+		self.RANGE_BASE_ADDR : list[int]
+		self.RANGE_ADDR_WIDTH : list[int]
 		
+				
 		# init Node object
 		super().__init__(asgn_addr_ranges, asgn_range_base_addr, asgn_range_addr_width, clock)
 
 		#check params
-		self.check_assign_params(data_dict)
+
+		
 
         # Must match a legal prefix exactly OR prefix + '_' + integer.
         # If duplicates appear, they must be numbered (TIM_0, TIM_1, ...).
-	def check_peripherals(self, peripherals: list):
+	def check_peripherals(self, peripherals: list[str]):
 		simply_v_crash = self.logger.simply_v_crash
 
 		seen = defaultdict(set)  # prefix -> set of used suffixes
@@ -65,6 +73,15 @@ class Bus(Node, ABC):
 
 	def check_assign_params(self, data_dict: dict):
 		simply_v_crash = self.logger.simply_v_crash
+
+		if("PROTOCOL" not in data_dict):
+			simply_v_crash("Protocol is mandatory")
+			
+		self.PROTOCOL = data_dict["PROTOCOL"]
+
+		# EARLY EXIT
+		if(self.PROTOCOL == "DISABLE"):
+			return
 		
 		if ("ID_WIDTH" in data_dict):
 			self.ID_WIDTH = int(data_dict["ID_WIDTH"])
@@ -111,8 +128,12 @@ class Bus(Node, ABC):
 			self.ADDR_RANGES = data_dict["ADDR_RANGES"]
 
 
+
 	def check_intra(self):
 		simply_v_crash = self.logger.simply_v_crash
+		MIN_AXI4_ADDR_WIDTH = 12
+		MIN_AXI4LITE_ADDR_WIDTH = 1
+
 
 		if (self.NUM_MI != len(self.RANGE_NAMES)):
 			simply_v_crash(f"The NUM_MI value {self.NUM_MI} does not match the number of RANGE_NAMES")
@@ -125,6 +146,15 @@ class Bus(Node, ABC):
 
 		if (self.NUM_SI != len(self.MASTER_NAMES)):
 			simply_v_crash(f"The NUM_SI does not match MASTER_NAMES in {self.MASTER_NAMES}")
+
+		# Check the minimum widths (AXI4 12, AXI4LITE 1)
+		for addr_width in self.RANGE_ADDR_WIDTH:
+			if addr_width > self.ADDR_WIDTH:
+				simply_v_crash(f"RANGE_ADDR_WIDTH is greater than {self.ADDR_WIDTH}")
+			if self.PROTOCOL == "AXI4" and addr_width < MIN_AXI4_ADDR_WIDTH:
+				simply_v_crash(f"RANGE_ADDR_WIDTH is less than {MIN_AXI4_ADDR_WIDTH}")
+			if self.PROTOCOL == "AXI4LITE" and addr_width < MIN_AXI4LITE_ADDR_WIDTH:
+				simply_v_crash(f"RANGE_ADDR_WIDTH is less than {MIN_AXI4LITE_ADDR_WIDTH}")
 		
 		# Check Addresses interactions
 		base_addresses = list()
@@ -155,12 +185,25 @@ class Bus(Node, ABC):
 		# the "ASGN" variables are the one assigned to the current bus
 		# not the one relatives to the nodes that we're trying to "allocate"
 		# on this bus
-
+		
+		#compute all ranges of the bus
+		bus_base_addresses = []
+		bus_end_addresses = []
 		for i in range(self.ASGN_ADDR_RANGES):
-			bus_base_address = self.ASGN_RANGE_BASE_ADDR[i]
+			bus_base_addresses.append(self.ASGN_RANGE_BASE_ADDR[i])
 			# RANGE_ADDR_WIDTH contained in the "Node" object of this bus
-			bus_end_address = bus_base_address + ~(~1 << (self.ASGN_RANGE_ADDR_WIDTH[i]-1))
+			bus_end_addresses.append(self.ASGN_RANGE_BASE_ADDR[i] + ~(~1 << (self.ASGN_RANGE_ADDR_WIDTH[i]-1)))
+
+		#for each node connected to the bus check if it's included in at least
+		#one bus address range
+		for base, end in zip(base_addresses,end_addresses):
+			included = 0
 			# Check if all the Nodes attached to this bus, fit in the assigned Address range
-			if((min(base_addresses) < bus_base_address) or (max(end_addresses) > bus_end_address)):
+			for bus_base, bus_end in zip(bus_base_addresses, bus_end_addresses):
+				if ((base >= bus_base) and (end <= bus_end)):
+					included = 1
+					break
+
+			if(included == 0):
 				simply_v_crash(f"The addresses assigned to some Node (Peripheral or Bus) in this bus\
 						don't fit in the address ranges assigned to this bus from his parent bus.")
--- config/scripts_ref/logger.py
+++ config/scripts_ref/logger.py
@@ -3,7 +3,7 @@ import logging
 
 class Logger():
 	def __init__(self, file_name: str):
-		self.file_name = file_name
+		self.file_name: str = file_name
 
 	def simply_v_crash(self, message: str):
 		logging.error(f"[{self.file_name}]")
