442ac363b72ede9c62a32f9a620936c15076a82f
Salvatore <santorosalvatoreunina@gmail.com>
2025-09-29 22:00:48 +0200

    Implemented HBUS with loopback functionality

--- config/scripts_ref/logger.py
+++ config/scripts_ref/logger.py
@@ -4,10 +4,12 @@ import logging
 class Logger():
 	def __init__(self, file_name: str):
 		self.file_name: str = file_name
+	
+	def simply_v_warning(self, message: str):
+		logging.warning(message+"\n")
 
 	def simply_v_crash(self, message: str):
-		logging.error(f"[{self.file_name}]")
-		logging.error(message)
+		logging.error(f"[{self.file_name}] \n---{message}---\n")
 		exit(1)
 
 
--- config/scripts_ref/node.py
+++ config/scripts_ref/node.py
@@ -5,6 +5,11 @@ class Node(ABC):
 		self.ASGN_ADDR_RANGES : int = asgn_addr_ranges 
 		self.ASGN_RANGE_BASE_ADDR : list[int] = asgn_range_base_addr 
 		self.ASGN_RANGE_ADDR_WIDTH : list[int] = asgn_range_addr_width 
+		self.ASGN_RANGE_END_ADDR : list[int] = []	# computed from ASGN_RANGE_BASE_ADDR and ASGN_RANGE_ADDR_WIDTH
 		self.CLOCK: int = clock
 		
+
+		for i in range(self.ASGN_ADDR_RANGES):
+			# RANGE_ADDR_WIDTH contained in the "Node" object of this bus
+			self.ASGN_RANGE_END_ADDR.append(self.ASGN_RANGE_BASE_ADDR[i] + ~(~1 << (self.ASGN_RANGE_ADDR_WIDTH[i]-1)))
 
