b70281ba12b9c2617eb7622342d3c3b6e6385d44
Salvatore <santorosalvatoreunina@gmail.com>
2025-10-08 12:35:09 +0200

    Integrating linker script generation

--- config/scripts_ref/peripheral.py
+++ config/scripts_ref/peripheral.py
@@ -5,6 +5,14 @@ class Peripheral(Node):
 			asgn_range_addr_width: list, clock: int):
 
 		self.LEGAL_CLOCK_DOMAIN: list[int] = []
+		self.IS_A_MEMORY: bool
 
 		super().__init__(name, asgn_addr_ranges, asgn_range_base_addr, asgn_range_addr_width, clock)
+
+		if any(sub in self.NAME for sub in ["BRAM", "DDR", "HBM"]):
+			self.IS_A_MEMORY = True
+		else:
+			self.IS_A_MEMORY = False
+
+
 		
